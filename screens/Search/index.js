6f396310d859668c0a26e3d7abfe86f59a200605
Thibault Guillard <tguillard@me.com>
2019-03-19 18:31:10 +0100

    Navigation Work

--- screens/Search/index.js
+++ screens/Search/index.js
@@ -1,6 +1,12 @@
 import {createAppContainer, createStackNavigator} from 'react-navigation'
 import SearchScreen from './SearchScreen'
+import SalleScreen from './SalleScreen'
 
 export default createStackNavigator({
-    SearchScreen,
-});
+    Search : SearchScreen,
+    Salle : SalleScreen
+},
+    {
+        initialRouteName: "Search"
+    }
+);
